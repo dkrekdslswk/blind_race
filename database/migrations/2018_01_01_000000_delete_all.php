3876c8f2f74b73906123fe2e93767b3f14226073
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-03-13 11:11:33 +0900

    cbc_database_test2

--- database/migrations/2018_01_01_000000_delete_all.php
+++ database/migrations/2018_01_01_000000_delete_all.php
@@ -17,8 +17,8 @@ class DeleteAll extends Migration
         Schema::dropIfExists('group_students');
 	DB::unprepared('DROP TRIGGER IF EXISTS tr_groups_user_division_check');
         Schema::dropIfExists('groups');
-        Schema::dropIfExists('user_division_keyword');
         Schema::dropIfExists('users');
+        Schema::dropIfExists('user_division_keyword');
     }
 
     /**
