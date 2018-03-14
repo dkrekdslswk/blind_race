33745148e6a4735817e639e2f1a36f7b9a39090e
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-03-14 11:44:25 +0900

    cbc_database_test5_8

--- database/migrations/2018_01_01_000000_delete_all.php
+++ database/migrations/2018_01_01_000000_delete_all.php
@@ -13,7 +13,7 @@ class DeleteAll extends Migration
      */
     public function up()
     {
-        Schema::dropIfExists('rece_results');
+        Schema::dropIfExists('race_results');
 
         Schema::dropIfExists('rece_set_exam');
         Schema::dropIfExists('rece_set_exam_state_keyword');
