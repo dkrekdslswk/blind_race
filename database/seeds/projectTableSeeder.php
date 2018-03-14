0f884d6c5fc4dcf392bb4b7849a4bbece29a1e74
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-03-14 20:17:02 +0900

    cbc_database_seed2

--- database/seeds/projectTableSeeder.php
+++ database/seeds/projectTableSeeder.php
@@ -11,8 +11,35 @@ class projectTableSeeder extends Seeder
      */
     public function run()
     {
-        $user = [
-
+        $users = [
+            ['tamp1','temp1','temp1','t'],
+            ['tamp2','temp2','temp2','s'],
+            ['tamp3','temp3','temp3','s'],
+            ['tamp4','temp4','temp4','s'],
+            ['tamp5','temp5','temp5','s'],
+            ['tamp6','temp6','temp6','s']
         ];
+ 
+        foreach($users as $user){
+            DB::table('users')->insert([
+                'user_id'       => $user[0];
+                'user_password' => $user[1];
+                'user_name'     => $user[2];
+                'user_division' => $user[3];
+            ]);
+        }
+
+        DB::table('groups')->insert([
+            'group_name'    => 'group1';
+            'user_t_num'    => 0;
+        ]);
+
+        for($user_num = 1 ; $user_num < 6 ; $user_num++){
+            DB::table('group_students')->insert([
+                'group_num'           => 0;
+                'user_num'            => $user_num;
+                'group_student_state' => 'a';
+            ]);
+        }
     }
 }
