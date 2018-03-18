2d0a785968f89e2fcd5c2a0babcf31f850d1abf3
PheonixAhn <ajk78978@gmail.com>
2018-03-18 16:35:36 +0900

    Nav , Racefiles
    상단 네비게이션 및 레이스리스트 UI 작업

--- resources/views/play_list/race_list.blade.php
+++ resources/views/play_list/race_list.blade.php
@@ -1,17 +1,34 @@
-          <div class="table-responsive">
-         <form id="search" class="form-inline my-2 my-lg-0 mr-lg-2">
+
+          
+          <div class="btn-process" style="margin-left:20%; margin-top:50px;">
+            <form id="" class="form-inline my-2 my-lg-0 mr-lg-2">
             <div class="input-group">
+          <select class="selectpicker btn btn-primary">
+            <optgroup label="검색옵션">
+              <option>제목</option>
+              <option>난이도</option>
+              <option>페이지</option>
+            </optgroup>
+          </select>
               <input class="form-control" type="text" placeholder="Search for...">
               <span class="input-group-append">
                 <button class="btn btn-primary" type="button">
-                  <i class="fa fa-search">search</i>
+                  <i class="fa fa-search">검색</i>
                 </button>
               </span>
+              
+            <button class="btn btn-info" style="margin-left:100px;">목록</button>
+            <button class="btn btn-info" style="margin-left:5px;">그룹선택</button>
             </div>
           </form>
-                <table class="" >
+          </div>
+          
+          
+          
+          <div class="table-responsive">
+              <table class="" >
             <thead>
-            <tr>
+            <tr class="bg-dark" style="height:40px">
                 <th>Name</th>
                 <th>Level</th>
                 <th>page</th>
@@ -48,7 +65,6 @@
           background: white;
           border-radius: 6px;
           overflow: hidden;
-          max-width: 800px;
           width: 100%;
           margin: 0 auto;
           position: relative;
@@ -79,7 +95,8 @@
         }
          .tb{
          }
-         #search{
-            margin-left:40%; 
+         .btn-process{
+           margin-left:40%%;
+           margin-bottom:40px;
          }
           </style>
