8ba0c19ec47d6172473e26dd7961b8cce6eacecf
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-03-23 20:29:14 +0900

    db new ver
    구문수정4

--- database/migrations/2018_03_09_205833_create_race_set_exam_table.php
+++ database/migrations/2018_03_09_205833_create_race_set_exam_table.php
@@ -29,7 +29,7 @@ class CreateRaceSetExamTable extends Migration
             $table->increments('set_exam_num');
 	    $table->unsignedInteger('group_num');
             $table->char('set_exam_state', 1);
-	    $table->foreign('set_exam_state')->references('keyword')->on('rece_set_exam_state_keyword');
+	    $table->foreign('set_exam_state')->references('keyword')->on('race_set_exam_state_keyword');
             $table->timestamps();
         });
     }
