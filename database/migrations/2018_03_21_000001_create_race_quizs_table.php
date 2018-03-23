f4f4ee8fd301490f75ea5bbf5432323fb6db3ddb
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-03-23 20:26:40 +0900

    db new ver
    구문수정 3

--- database/migrations/2018_03_21_000001_create_race_quizs_table.php
+++ database/migrations/2018_03_21_000001_create_race_quizs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRaceSetExamTable extends Migration
+class CreateRaceQuizsTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2018_03_21_000003_create_race_mistaken_quizs_table.php
+++ database/migrations/2018_03_21_000003_create_race_mistaken_quizs_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateRaceSetExamTable extends Migration
+class CreateRaceMistakenQuizsTable extends Migration
 {
     /**
      * Run the migrations.
