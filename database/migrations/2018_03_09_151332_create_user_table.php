97ce663b7a54890cdfc8b129e21b9aea23ba2e0d
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-03-28 22:31:03 +0900

    db update
    선생인가 학생인가 구분을
    user_division_keyword로 하고 있던것을
    user_teachers테이블로 변경

--- database/migrations/2018_03_09_151332_create_user_table.php
+++ database/migrations/2018_03_09_151332_create_user_table.php
@@ -13,25 +13,18 @@ class CreateUserTable extends Migration
      */
     public function up()
     {
-        Schema::create('user_division_keyword', function (Blueprint $table) {
-            $table->char('keyword', 1);
-	    $table->char('name', 32);
-	    $table->primary('keyword');
-        });
-
-        DB::table('user_division_keyword')->insert([
-	    ['keyword' => 't', 'name' => 'teacher'],
-	    ['keyword' => 's', 'name' => 'student']
-        ]);
-
         Schema::create('users', function (Blueprint $table) {
             $table->increments('user_num');
             $table->string('user_id', 16);
             $table->unique('user_id');
             $table->char('user_password', 160);
 	    $table->char('user_name', 20);
-            $table->char('user_division', 1);
-            $table->foreign('user_division')->references('keyword')->on('user_division_keyword');
+        });
+
+	Schema::create('user_teachers',function (Blueprint $table) {
+	    $table->unsignedInteger('user_t_num');
+	    $table->foreign('user_t_num')->references('user_num')->on('users');
+	    $table->primary('user_t_num');
         });
     }
 
--- database/migrations/2018_03_09_151714_create_race_table.php
+++ database/migrations/2018_03_09_151714_create_race_table.php
@@ -16,27 +16,11 @@ class CreateRaceTable extends Migration
         Schema::create('races', function (Blueprint $table) {
             $table->increments('race_num');
 	    $table->unsignedInteger('user_t_num');
-	    $table->foreign('user_t_num')->references('user_num')->on('users');
+	    $table->foreign('user_t_num')->references('user_t_num')->on('user_teachers');
             $table->string('race_name', 100);
             $table->string('race_folder_name', 100);
             $table->timestamps();
         });
-
-	DB::unprepared('
-	CREATE TRIGGER tr_races_user_division_check BEFORE INSERT ON races
-	FOR EACH ROW
-	    BEGIN
-                DECLARE v_division CHAR(1) DEFAULT NULL;
-
-		SELECT user_division INTO v_division
-		FROM users
-		WHERE user_num = NEW.user_t_num;
-		IF v_division <> "t" THEN
-			SIGNAL SQLSTATE "15000"
-				SET MESSAGE_TEXT = "check constraint on races.user_t_num division failed";
-		END IF;
-	    END;
-	');
     }
 
     /**
