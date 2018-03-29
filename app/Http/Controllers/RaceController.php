76e95dc60563754b4a49118d555563166a60afc9
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-03-29 21:42:06 +0900

    cbc controller test...

--- app/Http/Controllers/RaceController.php
+++ app/Http/Controllers/RaceController.php
@@ -1,4 +1,4 @@
-<?php
+﻿<?php
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\DB;
@@ -26,7 +26,7 @@ class RaceController extends Controller
     {
         //$json     = $request->input('post');
         $json     = '{"group":["groupId":1],"race":["raceMode":"n","raceCount":30,"raceId":1]}';
-        $postData = json_decode($json);
+        $postData = json_decode($json, true);
 
 	// ���� ������
         $session['user_num']   = 1;
@@ -38,22 +38,22 @@ class RaceController extends Controller
 			'groups.group_name as groupName',
 			DB::raw('COUNT(group_students.user_num) as studentCount')])
 		->join('group_students', 'group_students.group_num', '=', 'groups.group_num')
-		->where(['groups.group_num', '=', $postData.group['groupId'],
+		->where(['groups.group_num', '=', $postData['group']['groupId'],
 		['groups.user_t_num', '=', $_session['user_num']]])
 		->group_by('groups.group_num')
 		->get();
 
         $raceSetExamId = DB::table('race_set_exam')->insertGetId([
 		'group_num'=>$groupData.groupId, 
-		'set_exam_state'=>$postData.race['raceMode'], 
-		'exam_count'=>$postData.race['examCount'], 
-		'set_exam_data'=>'{"base":"'.$postData.race['raceId'].'","bookPage":null}'
+		'set_exam_state'=>$postData['race']['raceMode'], 
+		'exam_count'=>$postData['race']['examCount'], 
+		'set_exam_data'=>'{"base":"'.$postData['race']['raceId'].'","bookPage":null}'
 		], 'set_exam_num');
         $session['set_exam_num'] = $raceSetExamId;
 
 	$raceName = DB::table('races')
 		->select('race_name')
-		->where('race_num', '=', $postData.race['raceId'])
+		->where('race_num', '=', $postData['race']['raceId'])
 		->value('race_name');
 
 	$returnValue = array(
