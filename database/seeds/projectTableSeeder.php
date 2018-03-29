6ce2f8ec4dfe8896cf5bc44d7cf07b38c5be3b2b
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-03-29 14:11:57 +0900

    db seed insert
    데이터베이스 seed값

--- database/seeds/projectTableSeeder.php
+++ database/seeds/projectTableSeeder.php
@@ -21,32 +21,42 @@ class projectTableSeeder extends Seeder
         ];
  
         foreach($users as $user){
-            DB::table('users')->insert([
+            $userId = DB::table('users')->insertGetId([
                 'user_id'       => array_get($user, 0),
                 'user_password' => array_get($user, 1),
                 'user_name'     => array_get($user, 2)
-            ]);
+            ], 'user_num');
+
+            if(!isset($user_first)){
+                $userFirstId = $userId;
+            }
         }
 
         DB::table('user_teachers')->insert([
-            'user_t_num'    => 1
+            'user_t_num'    => $userFirstId
         ]);
 
-        DB::table('groups')->insert([
+        $groupId = DB::table('groups')->insertGetId([
             'group_name'    => 'group1',
-            'user_t_num'    => 1
-        ]);
+            'user_t_num'    => $userFirstId
+        ], 'group_num');
 
         for($user_num = 2 ; $user_num <= 6 ; $user_num++){
             DB::table('group_students')->insert([
-                'group_num'           => 1,
+                'group_num'           => $groupId,
                 'user_num'            => $user_num,
                 'group_student_state' => 'a'
             ]);
         }
 
+        $raceId = DB::table('races')->insertGetId([
+            'user_t_num'=>$userFirstId,
+            'race_name'=>"테스트용 레이스1",
+            'race_folder_name'=>'-';
+        ], 'race_num');
+
         for($quiz_count = 1 ; $quiz_count <= 30 ; $quiz_count++){
-	    DB::table()->insert([
+	    $quizId = DB::table('quiz_bank')->insertGetId([
                 'quiz_question'=>''.($quiz_count%4+1).'',
                 'quiz_right_answer'=>''.($quiz_count%4+1).'',
                 'quiz_example1'=>''.(($quiz_count+1)%4+1).'',
@@ -54,9 +64,12 @@ class projectTableSeeder extends Seeder
                 'quiz_example3'=>''.(($quiz_count+3)%4+1).'',
                 'quiz_type'=>'o',
                 'quiz_level'=>'5';
+            ], 'quiz_num');
+
+            DB::table('race_quizs')->insert([
+                'race_num'=>$raceId,
+                'quiz_num'=>$quizId
             ]);
 	}
-
-        
     }
 }
