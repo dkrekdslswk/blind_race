4f298bf992d99222cfdc0542f55aa0c2108d3956
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-04-04 23:43:04 +0900

    test

--- resources/views/cbcSocketTest.blade.php
+++ resources/views/cbcSocketTest.blade.php
@@ -8,21 +8,25 @@
     <form action="{{url('raceController/create')}}"  method="Post" enctype="multipart/form-data">
         {{csrf_field()}}
         <h3 class="form-section">Person Info</h3>
+        <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
         <button type="submit">傈价</button>
     </form>
     <form action="{{url('raceController/teacherIn')}}"  method="Post" enctype="multipart/form-data">
         {{csrf_field()}}
         <h3 class="form-section">Person Info</h3>
+        <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
         <button type="submit">傈价</button>
     </form>
     <form action="{{url('raceController/studentIn')}}"  method="Post" enctype="multipart/form-data">
         {{csrf_field()}}
         <h3 class="form-section">Person Info</h3>
+        <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
         <button type="submit">傈价</button>
     </form>
     <form action="{{url('raceController/quizNext')}}"  method="Post" enctype="multipart/form-data">
         {{csrf_field()}}
         <h3 class="form-section">Person Info</h3>
+        <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
         <button type="submit">傈价</button>
     </form>
       
