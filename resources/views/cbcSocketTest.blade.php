fea699f6927a8a926cf40ee7fd99efc2728b9974
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-04-04 23:36:45 +0900

    test

--- resources/views/cbcSocketTest.blade.php
+++ resources/views/cbcSocketTest.blade.php
@@ -5,7 +5,26 @@
     <title>Chatroom</title>
 </head>
 <body>
-    
+    <form action="{{url('raceController/create')}}"  method="Post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <h3 class="form-section">Person Info</h3>
+        <button type="submit">傈价</button>
+    </form>
+    <form action="{{url('raceController/teacherIn')}}"  method="Post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <h3 class="form-section">Person Info</h3>
+        <button type="submit">傈价</button>
+    </form>
+    <form action="{{url('raceController/studentIn')}}"  method="Post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <h3 class="form-section">Person Info</h3>
+        <button type="submit">傈价</button>
+    </form>
+    <form action="{{url('raceController/quizNext')}}"  method="Post" enctype="multipart/form-data">
+        {{csrf_field()}}
+        <h3 class="form-section">Person Info</h3>
+        <button type="submit">傈价</button>
+    </form>
       
 </body>
 </html>
