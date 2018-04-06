0406aa407d5a452f394d21f98b854bc7b251b747
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-04-06 22:06:38 +0900

    raceController 구문수정

--- app/Http/Controllers/RaceController.php
+++ app/Http/Controllers/RaceController.php
@@ -33,7 +33,7 @@ class RaceController extends Controller
         $userId = DB::table('users as u')
             ->select(['u.user_num   as user_num',
                     's.session_num  as session_num'])
-            ->where('u.user_id', '=', 'tamp1id')
+            ->where('u.user_num', '=', 'tamp1id')
             ->leftJoin('sessions as s', 's.user_num', '=', 'u.user_num')
             ->first();
 
@@ -158,7 +158,7 @@ class RaceController extends Controller
         $userId = DB::table('users as u')
             ->select(['u.user_num   as user_num',
                     's.session_num  as session_num'])
-            ->where('s.user_id', '=', $postData['userId'])
+            ->where('s.user_num', '=', $postData['userId'])
             ->leftJoin('sessions as s', 's.user_num', '=', 'u.user_num')
             ->first();
 
@@ -232,9 +232,9 @@ class RaceController extends Controller
 
         // test
         $userId = DB::table('users as u')
-            ->select(['u.user_num   as user_num',
-                's.session_num  as session_num'])
-            ->where('s.user_id', '=', $postData['userId'])
+            ->select(['u.user_num as user_num',
+                's.session_num as session_num'])
+            ->where('s.user_num', '=', $postData['userId'])
             ->leftJoin('sessions as s', 's.user_num', '=', 'u.user_num')
             ->first();
 
