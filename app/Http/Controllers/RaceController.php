b929de38d8cec503ae2b133b30331511da5bbb2b
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-04-06 10:53:22 +0900

    ㅅ

--- app/Http/Controllers/RaceController.php
+++ app/Http/Controllers/RaceController.php
@@ -236,8 +236,8 @@ class RaceController extends Controller
                                'rse.book_num                 as bookId', 
                                'rse.book_page_start          as pageStart', 
                                'rse.book_page_end            as pageEnd',
-                               'rse.exam_count               as setExamCount',
-			                   /* DB::raw('COUNT(quiz.sequence) as examCount')*/)
+                               'rse.exam_count               as setExamCount'/*,
+			                    DB::raw('COUNT(quiz.sequence) as examCount')*/)
                       ->where('rse.set_exam_num', '=', $postData['setExamId'])
                       /*->leftJoin('race_set_exam_quizs as quiz', 'quiz.set_exam_num', '=', 'rse.set_exam_num')
                       ->groupBy('res.set_exam_num')*/
