66ab32715ac378f6bf86d73b244d1478511a448d
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-04-06 21:37:19 +0900

    cbcteat

--- resources/views/cbcSocketTest.blade.php
+++ resources/views/cbcSocketTest.blade.php
@@ -5,7 +5,29 @@
     <title>Chatroom</title>
 </head>
 <body>
-    
-      
+<form action="{{url('raceController/create')}}"  method="Post" enctype="multipart/form-data">
+    {{csrf_field()}}
+    <h3 class="form-section">Person Info</h3>
+    <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
+    <button type="submit">����</button>
+</form>
+<form action="{{url('raceController/teacherIn')}}"  method="Post" enctype="multipart/form-data">
+    {{csrf_field()}}
+    <h3 class="form-section">Person Info</h3>
+    <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
+    <button type="submit">����</button>
+</form>
+<form action="{{url('raceController/studentIn')}}"  method="Post" enctype="multipart/form-data">
+    {{csrf_field()}}
+    <h3 class="form-section">Person Info</h3>
+    <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
+    <button type="submit">����</button>
+</form>
+<form action="{{url('raceController/quizNext')}}"  method="Post" enctype="multipart/form-data">
+    {{csrf_field()}}
+    <h3 class="form-section">Person Info</h3>
+    <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
+    <button type="submit">����</button>
+</form>
 </body>
 </html>
