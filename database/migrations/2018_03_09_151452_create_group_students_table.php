79c0aedeee91501d7ed6b2f91c5dda7eb0fc0a81
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-04-06 15:49:53 +0900

    Revert "save"
    This reverts commit 3f3375d4d2b90be0482cab9888107842016292da.

--- database/migrations/2018_03_09_151452_create_group_students_table.php
+++ database/migrations/2018_03_09_151452_create_group_students_table.php
@@ -17,26 +17,26 @@ class CreateGroupStudentsTable extends Migration
 
         Schema::create('group_student_state_keyword', function (Blueprint $table) {
             $table->char('keyword', 1);
-	        $table->string('name', 32);
-	        $table->primary('keyword');
+	    $table->string('name', 32);
+	    $table->primary('keyword');
         });
 
         DB::table('group_student_state_keyword')->insert([
-	        ['keyword' => 'a', 'name' => 'active'],
-	        ['keyword' => 'p', 'name' => 'application'],
-	        ['keyword' => 'r', 'name' => 'refusal']
+	    ['keyword' => 'a', 'name' => 'active'],
+	    ['keyword' => 'p', 'name' => 'application'],
+	    ['keyword' => 'r', 'name' => 'refusal']
         ]);
 
         Schema::create('group_students', function (Blueprint $table) {
-	        $table->unsignedInteger('group_num');
-	        $table->foreign('group_num')->references('group_num')->on('groups');
+	    $table->unsignedInteger('group_num');
+	    $table->foreign('group_num')->references('group_num')->on('groups');
 
-	        $table->unsignedInteger('user_num');
-	        $table->foreign('user_num')->references('user_num')->on('users');
-	        $table->primary(['group_num', 'user_num']);
+	    $table->unsignedInteger('user_num');
+	    $table->foreign('user_num')->references('user_num')->on('users');
+	    $table->primary(['group_num', 'user_num']);
 
             $table->char('group_student_state', 1);
-	        $table->foreign('group_student_state')->references('keyword')->on('group_student_state_keyword');
+	    $table->foreign('group_student_state')->references('keyword')->on('group_student_state_keyword');
 
             $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
         });
--- database/migrations/2018_03_09_205804_create_quiz_bank_table.php
+++ database/migrations/2018_03_09_205804_create_quiz_bank_table.php
@@ -15,38 +15,38 @@ class CreateQuizBankTable extends Migration
     {
         Schema::create('quiz_type_keyword', function (Blueprint $table) {
             $table->char('keyword', 1);
-	        $table->string('name', 32);
-	        $table->primary('keyword');
+	    $table->string('name', 32);
+	    $table->primary('keyword');
         });
 
         DB::table('quiz_type_keyword')->insert([
-	        ['keyword' => 'o', 'name' => 'objective'],
-	        ['keyword' => 's', 'name' => 'subjective'],
-	        ['keyword' => 't', 'name' => 'set word'],
-	        ['keyword' => 'w', 'name' => 'word']
+	    ['keyword' => 'o', 'name' => 'objective'],
+	    ['keyword' => 's', 'name' => 'subjective'],
+	    ['keyword' => 't', 'name' => 'set word'],
+	    ['keyword' => 'w', 'name' => 'word']
         ]);
 
         Schema::create('quiz_bank', function (Blueprint $table) {
             $table->increments('quiz_num');
 
-	        $table->unsignedInteger('book_num')->nullable();
-	        $table->foreign('book_num')->references('book_num')->on('books');
+	    $table->unsignedInteger('book_num')->nullable();
+	    $table->foreign('book_num')->references('book_num')->on('books');
 
-	        $table->unsignedSmallInteger('book_page')->nullable();
+	    $table->unsignedSmallInteger('book_page')->nullable();
 
-	        $table->string('quiz_question',1000);
-	        $table->string('quiz_right_answer',100);
-	        $table->string('quiz_example1',100)->nullable();
-	        $table->string('quiz_example2',100)->nullable();
-	        $table->string('quiz_example3',100)->nullable();
+	    $table->string('quiz_question',1000);
+	    $table->string('quiz_right_answer',100);
+	    $table->string('quiz_example1',100)->nullable();
+	    $table->string('quiz_example2',100)->nullable();
+	    $table->string('quiz_example3',100)->nullable();
 
             $table->char('quiz_type', 1);
-	        $table->foreign('quiz_type')->references('keyword')->on('quiz_type_keyword');
+	    $table->foreign('quiz_type')->references('keyword')->on('quiz_type_keyword');
 
             $table->char('quiz_level', 1);
 
-	        $table->unsignedInteger('user_t_num')->nullable();
-	        $table->foreign('user_t_num')->references('user_t_num')->on('user_teachers');
+	    $table->unsignedInteger('user_t_num')->nullable();
+	    $table->foreign('user_t_num')->references('user_t_num')->on('user_teachers');
         });
     }
 
