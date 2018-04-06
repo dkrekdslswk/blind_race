34aefc3e8e8d2b63b692e95c64729047426b7b79
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-04-06 22:00:13 +0900

    cbc test view update

--- resources/views/cbcSocketTest.blade.php
+++ resources/views/cbcSocketTest.blade.php
@@ -9,25 +9,31 @@
     {{csrf_field()}}
     <h3 class="form-section">Person Info</h3>
     <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
-    <button type="submit">����</button>
+    <button type="submit">create</button>
 </form>
 <form action="{{url('raceController/teacherIn')}}"  method="Post" enctype="multipart/form-data">
     {{csrf_field()}}
     <h3 class="form-section">Person Info</h3>
     <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
-    <button type="submit">����</button>
+    <button type="submit">teacherIn</button>
 </form>
 <form action="{{url('raceController/studentIn')}}"  method="Post" enctype="multipart/form-data">
     {{csrf_field()}}
     <h3 class="form-section">Person Info</h3>
     <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
-    <button type="submit">����</button>
+    <button type="submit">studentIn</button>
 </form>
 <form action="{{url('raceController/quizNext')}}"  method="Post" enctype="multipart/form-data">
     {{csrf_field()}}
     <h3 class="form-section">Person Info</h3>
     <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
-    <button type="submit">����</button>
+    <button type="submit">quizNext</button>
+</form>
+<form action="{{url('raceController/quizNext')}}"  method="Post" enctype="multipart/form-data">
+    {{csrf_field()}}
+    <h3 class="form-section">Person Info</h3>
+    <input type="text" name="post" id="post" class="form-control first_name"  placeholder="First Name">
+    <button type="submit">quizNext</button>
 </form>
 </body>
 </html>
