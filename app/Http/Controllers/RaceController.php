65f10719767f35947953bcebe4ef0ab92dd16d0a
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-04-06 10:10:07 +0900

    구문수정

--- app/Http/Controllers/RaceController.php
+++ app/Http/Controllers/RaceController.php
@@ -232,18 +232,18 @@ class RaceController extends Controller
                   ->first();
 
         if(isset($chaeck->session_num)){
-            $returnValue = DB::table('race_set_exam as rse')
+            $raceId = DB::table('race_set_exam as rse')
                       ->select('rse.race_num                 as base', 
                                'rse.book_num                 as bookId', 
                                'rse.book_page_start          as pageStart', 
                                'rse.book_page_end            as pageEnd',
                                'rse.exam_count               as setExamCount',
 			       DB::raw('COUNT(quiz.sequence) as examCount'))
-                      ->where('rse.set_exam_num', '=', $postData['setExamId'])
+                      ->where('quiz.set_exam_num', '=', $postData['setExamId'])
                       ->join('race_set_exam_quizs as quiz', 'quiz.set_exam_num', '=', 'rse.set_exam_num')
                       ->groupBy('res.set_exam_num')
                       ->first();
-/*$raceId
+
             $setExams = DB::table('race_quizs as rq')
                         ->select('rq.quiz_num as quiz_num)
                         ->where(['rq.race_num'       => $raceId->base,
@@ -267,7 +267,7 @@ class RaceController extends Controller
                         ->join('quiz_bank as qb', 'qb.quiz_num', 'rq.quiz_num')
                         ->inRandomOrder()
                         ->first();
-*/
+
         } else {
             $returnValue = "fail";
         }
