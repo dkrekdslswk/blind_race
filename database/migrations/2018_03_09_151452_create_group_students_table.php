beb748f3ace0d9b893b082469f694377074431d4
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-04-06 15:42:52 +0900

    save

--- database/migrations/2018_03_09_151452_create_group_students_table.php
+++ database/migrations/2018_03_09_151452_create_group_students_table.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateGroupStudentTable extends Migration
+class CreateGroupStudentsTable extends Migration
 {
     /**
      * Run the migrations.
