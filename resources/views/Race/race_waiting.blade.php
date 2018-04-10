ce8aa1db677675f9138ceb627bf704712d202acd
LilMinsu <smilemsu@naver.com>
2018-04-11 02:07:50 +0900

    홈페이지 수정

--- resources/views/Race/race_waiting.blade.php
+++ resources/views/Race/race_waiting.blade.php
@@ -75,7 +75,7 @@
 
             $('#wait_room').hide();
             $('#playing_contents').show();
-            var socket = io(':8890'); //1
+            var socket = io(':8890'); //14
             socket.emit('join', pub_group_num);
             //아아아
             var quiz_number = 0;
@@ -276,6 +276,7 @@
 
 <div id="wait_room">
     <div class="student">
+        
 
         <!--<form action="">-->
         <button onclick="btn_click();" id="start_btn" class="btn btn-lg btn-primary" style="">시작하기</button>
--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -15,7 +15,7 @@
 
 
 			<header id="header" class="alt">
-				<div class="logo"><a href="index.html">十分十分 <span></span></a></div>
+				<div class="logo"><a href="#">十分十分 <span></span></a></div>
 				<a href="#menu"><?php echo session()->get('connect_num','')." 번 로그인"; ?></a>
 			</header>
 
@@ -48,10 +48,10 @@
 					</div>
 				</article>
 				<article>
-					<img src="img/mainimages/slide03.jpg"  alt="" />
+					<img src="img/mainimages/slide03.png"  alt="" />
 					<div class="inner">
 						<header>
-							<h2>일본어 학습 도우미</h2>
+							<h2></h2>
 						</header>
 					</div>
 				</article>
@@ -67,7 +67,7 @@
 					<img src="img/mainimages/slide05.jpg"  alt="" />
 					<div class="inner">
 						<header>
-							<h2>일본어 학습 도우미</h2>
+							
 						</header>
 					</div>
 				</article>
@@ -88,7 +88,7 @@
 										<p>학생들에게 조금 더 쉽게 가르칠수 있습니다.</p>
 										<h2>나의 그룹</h2>
 									</header>
-									<p> Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada.</p>
+									<p> </p>
 									<footer class="align-center">
 										<a href="/mygroup" class="button alt">입장하기</a>
 									</footer>
@@ -100,14 +100,14 @@
 						<div>
 							<div class="box">
 								<div class="image fit">
-									<img src="img/mainimages/student.jpg" width="600" height="400" alt="" />
+									<img src="img/mainimages/teacher2.jpg" width="600" height="400" alt="" />
 								</div>
 								<div class="content">
 									<header class="align-center">
-										<p>플레이하며 효과적으로 배울수 있습니다.</p>
-										<h2>레이스</h2>
+										<p>수업시간 내 익명으로 즐기는 실시간 수업 내용 학습 퀴즈</p>
+										<h2>Race</h2>
 									</header>
-									<p> Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada.</p>
+									<p> </p>
 									<footer class="align-center">
 										<a href="/race" class="button alt">입장하기</a>
 									</footer>
@@ -118,14 +118,14 @@
 						<div>
 							<div class="box">
 								<div class="image fit">
-									<img src="img/mainimages/student.jpg" width="600" height="400" alt="" />
+									<img src="img/mainimages/teacher3.jpg" width="600" height="400" alt="" />
 								</div>
 								<div class="content">
 									<header class="align-center">
-										<p>플레이하며 효과적으로 배울수 있습니다.</p>
-										<h2>레이드</h2>
+										<p>팀 형식의 협동 경쟁 모드</p>
+										<h2>Raid</h2>
 									</header>
-									<p> Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada.</p>
+									<p> </p>
 									<footer class="align-center">
 										<a href="/raid" class="button alt">입장하기</a>
 									</footer>
@@ -136,14 +136,14 @@
 						<div>
 							<div class="box">
 								<div class="image fit">
-									<img src="img/mainimages/student.jpg" width="600" height="400" alt="" />
+									<img src="img/mainimages/teacher4.jpg" width="600" height="400" alt="" />
 								</div>
 								<div class="content">
 									<header class="align-center">
-										<p>플레이하며 효과적으로 배울수 있습니다.</p>
+										<p>교수들을 위한 간편한 시험 출제</p>
 										<h2>문제나무</h2>
 									</header>
-									<p> Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada.</p>
+									<p> </p>
 									<footer class="align-center">
 										<a href="/Quiz_tree" class="button alt">입장하기</a>
 									</footer>
@@ -158,10 +158,10 @@
 								</div>
 								<div class="content">
 									<header class="align-center">
-										<p>플레이하며 효과적으로 배울수 있습니다.</p>
-										<h2>레코드s sds박스</h2>
+										<p>학생의 학습기록과 시험결과를 실시간으로 기록</p>
+										<h2>Record Box</h2>
 									</header>
-									<p> Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada.</p>
+									<p> </p>
 									<footer class="align-center">
 										<a href="recordbox" class="button alt">입장하기</a>
 									</footer>
@@ -172,14 +172,14 @@
 						<div>
 							<div class="box">
 								<div class="image fit">
-									<img src="img/mainimages/student.jpg" width="600" height="400" alt="" />
+									<img src="img/mainimages/teacher5.jpg" width="600" height="400" alt="" />
 								</div>
 								<div class="content">
 									<header class="align-center">
-										<p>플레이하며 효과적으로 배울수 있습니다.</p>
-										<h2>피드백</h2>
+										<p>학생들의 의견을 피드백하여 알려줄수 있습니다.</p>
+										<h2>Feedback</h2>
 									</header>
-									<p> Cras aliquet urna ut sapien tincidunt, quis malesuada elit facilisis. Vestibulum sit amet tortor velit. Nam elementum nibh a libero pharetra elementum. Maecenas feugiat ex purus, quis volutpat lacus placerat malesuada.</p>
+									<p> </p>
 									<footer class="align-center">
 										<a href="Feedback" class="button alt">입장하기</a>
 									</footer>
