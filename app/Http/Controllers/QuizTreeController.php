613d6b1f60457b6cab265eada6827d964e974b58
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-04-16 09:19:29 +0900

    test

--- app/Http/Controllers/QuizTreeController.php
+++ app/Http/Controllers/QuizTreeController.php
@@ -15,7 +15,7 @@ class QuizTreeController extends Controller
 //        $json     = $request->input('post');
 //        $json     = json_encode(array('folderId' => null));
 //        $postData = json_decode($json);
-        $postData = array('folderId' => $folderId == 0 ? '' : $folderId);
+        $postData = array('folderId' => $folderId == 'null' ? '' : $folderId);
 
         // test 임시로 유저 세션 부여
         $userData = DB::table('users as u')
@@ -51,7 +51,7 @@ class QuizTreeController extends Controller
 //        $json     = $request->input('post');
 //        $json     = json_encode(array('folderId' => null));
 //        $postData = json_decode($json);
-        $postData = array('folderId' => $folderId == 0 ? '' : $folderId);
+        $postData = array('folderId' => $folderId == 'null' ? '' : $folderId);
 
         // test 임시로 유저 세션 부여
         $userData = DB::table('users as u')
@@ -79,7 +79,7 @@ class QuizTreeController extends Controller
             'selectFolder' => $postData['folderId']);
 
 //        return $returnValue;
-        return view('play_list/race_list')->with('response', $returnValue);
+        return view('Race/race')->with('response', $returnValue);
     }
 
     public function folderGet(){
