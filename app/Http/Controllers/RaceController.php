63d5b62d8d7abab43e97716c316a6cd25cf7ef98
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-04-27 22:12:30 +0900

    test

--- app/Http/Controllers/RaceController.php
+++ app/Http/Controllers/RaceController.php
@@ -215,7 +215,7 @@ class RaceController extends Controller{
             'characterId'       => $postData['characterId']
         );
 
-        return $data;
+        return $returnValue;
     }
 
     // get quiz
