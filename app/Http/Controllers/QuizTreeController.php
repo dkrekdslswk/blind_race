07bd9e404d31620bd0dd6ddce0fc427aa000e648
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-05-09 13:33:19 +0900

    구문수정

--- app/Http/Controllers/QuizTreeController.php
+++ app/Http/Controllers/QuizTreeController.php
@@ -525,10 +525,11 @@ class QuizTreeController extends Controller
     private function deleteListQuiz($listId){
         // 문제 리스트 받아오기
         $listQuizs = DB::table('listQuizs')
+            ->select('quizNumber as number')
             ->where([
                 'listNumber' => $listId
             ])
-            ->pluck('quizNumber')
+            ->get()
             ->toArray();
 
         // 문제 리스트 삭제
