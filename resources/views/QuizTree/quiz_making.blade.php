5b1d459c749578058c6bde3bac3c4739782f5af0
Yoolme <shim1556@naver.com>
2018-05-11 23:34:18 +0900

    quizTree 임시저장
    전반적인 주석 추가
    임시저장용

--- resources/views/QuizTree/quiz_making.blade.php
+++ resources/views/QuizTree/quiz_making.blade.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -26,12 +25,37 @@
             margin: 10px 20px 10px 0px;
             text-align: right;
         }
+
+        .btn-lg {
+            font-size: 18px;
+            line-height: 1.33;
+            border-radius: 6px;
+        }
+
+        .btn.outline {
+            background: none;
+            padding: 12px 22px;
+        }
+        .btn-primary.outline {
+            border: 2px solid #0099cc;
+            color: #0099cc;
+        }
+
+
+
     </style>
 </head>
 
+<!--1. 퀴즈를 새로 생성할 경우-->
+@if(count($response['quizs']) == 0)
+    <body onload="document.getElementById('add').click();">
 
+<!--2. 퀴즈를 수정할 경우-->
+@elseif(count($response['quizs']) > 0)
+    <body onload="updateQuiz()">
 
-<body onload="document.getElementById('add').click();">
+    @endif
+
 <nav>
     @include('Navigation.main_nav')
 </nav>
@@ -43,17 +67,23 @@
 <script>
 
     var idNum = 0;
+
+    // 문항 번호(id) 저장용 배열
     var idArray = new Array();
 
+    // 출제 유형 저장용 배열
     var makeTypeRadio = new Array();
+    // 문제 유형 저장용 배열
     var quizTypeRadio = new Array();
 
+    // 메인 -> 문제 테이블 추가 : empty, update
     function quizAdd(addArr) {
 
-        // id 값 부여
+        // id 값 부여 + 배열에 저장
         idNum++;
         idArray.push(idNum);
 
+        // quizBox Div에 table 추가
         $(".quizBox").append(
             "<div class='quiz' style='margin: 20px'>" +
             "<table class='table table-bordered' id='tableNum"+ idNum +"'>" +
@@ -108,7 +138,7 @@
             "</div>");
 
 
-        // 받아온 값으로 라디오버튼 표시
+        // 받아온 값(교재 정보 or 수정 문제)으로 라디오버튼 표시
         if(addArr.makeType == "obj") {
             $("#obj" + idNum).attr("checked", true);
             makeTypeRadio[idNum] = addArr.makeType;
@@ -140,11 +170,13 @@
         // 라디오버튼 선택 확인 (출제유형)
         $("input[name='makeType" + idNum + "']:radio").change(function () {
 
-            // 테이블 변형
+            // 테이블 변형 : 주관식일 경우 addSub() 메서드 호출
             if(this.id.slice(0,3) == "sub") {
                 makeTypeRadio[this.id.slice(3)] = "sub";
                 addSub(this.id.slice(3), addArr);
             }
+
+            // 테이블 변형 : 객관식일 경우 addObj() 메서드 호출
             else if (this.id.slice(0,3) == "obj") {
                 makeTypeRadio[this.id.slice(3)] = "obj";
                 addObj(this.id.slice(3), addArr);
@@ -166,7 +198,7 @@
         });
 
 
-        // 퀴즈 삭제
+        // 문항 삭제 버튼 클릭 시
         $(document).on('click', '#deleteNum'+idNum, function (e) {
             e.preventDefault();
 
@@ -187,7 +219,7 @@
         });
     }
 
-    // 테이블 : 객관식 용
+    // 테이블 : 객관식 용 (칸 4개)
     function addObj(idNum, addArr) {
         $('#addTr' + idNum).empty();
 
@@ -212,7 +244,7 @@
         );
     }
 
-    // 테이블 : 주관식 용
+    // 테이블 : 주관식 용 (칸 2개)
     function addSub(idNum, addArr) {
         $('#addTr' + idNum).empty();
 
@@ -231,10 +263,11 @@
         );
     }
 
-    // 문항 추가
+    // 문항 추가 버튼 클릭 시 + 퀴즈 생성(BODY ONLOAD)
     $(document).on('click', '#add', function (e) {
         e.preventDefault();
 
+        // 새로운 퀴즈
         var emptyArray = {
             question: "",
             right: "",
@@ -250,8 +283,7 @@
 
     });
 
-
-    // 퀴즈 저장
+    // 퀴즈 저장 버튼 클릭 시
     $(document).on('click', '#save', function (e) {
         e.preventDefault();
 
@@ -303,37 +335,79 @@
                 }
             },
             error: function (data) {
-                alert("출제 유형과 문제 유형을 모두 입력해주세요");
+                alert("error");
             }
         });
 
     });
 
+    // 수정 퀴즈(BODY ONLOAD) : 기존에 저장된 문제 목록 불러오기
+    function updateQuiz() {
+
+        @for($i = count($response['quizs']) - 1; $i >= 0; $i--)
+
+        var updateArray = {
+            question: "{{$response['quizs'][$i]['question']}}",
+            right: "{{$response['quizs'][$i]['right']}}",
+            example1: "{{$response['quizs'][$i]['example1']}}",
+            example2: "{{$response['quizs'][$i]['example2']}}",
+            example3: "{{$response['quizs'][$i]['example3']}}",
+            makeType: "{{$response['quizs'][$i]['makeType']}}",
+            quizType: "{{$response['quizs'][$i]['quizType']}}",
+            hint: "{{$response['quizs'][$i]['hint']}}"
+        };
+
+        quizAdd(updateArray);
+
+        @endfor
+    };
+
+    // 목록으로 버튼 클릭 시 : quiz_list로 돌아가기
+    function backToList() {
+        window.location.href = "{{url('quiz_list')}}";
+    }
+
     // 테스트
-//    $(document).on('click', '#test', function (e) {
-//        e.preventDefault();
-//
-//        var quizs = new Array();
-//
-//        for (var i in idArray) {
-//            quizs.push({
-//                makeType: makeTypeRadio[idArray[i]],
-//                quizType: quizTypeRadio[idArray[i]]
-//            });
-//        }
-//
-//        alert(JSON.stringify(quizs));
-//    });
+    $(document).on('click', '#test', function (e) {
+        e.preventDefault();
+
+        var listId = "{{$response['listId']}}";
+
+        var quizs = new Array();
+
+        for (var i in idArray) {
+            quizs.push({
+                question: $('#question' + idArray[i]).val(),
+                right: $('#right' + idArray[i]).val(),
+                example1: $('#example1' + idArray[i]).val(),
+                example2: $('#example2' + idArray[i]).val(),
+                example3: $('#example3' + idArray[i]).val(),
+                makeType: makeTypeRadio[idArray[i]],
+                quizType: quizTypeRadio[idArray[i]],
+                hint: $('#hint' + idArray[i]).val()
+            });
+        }
+
+        alert(listId);
+        alert(JSON.stringify(quizs));
+    });
 
 </script>
 
+
 <div class="contents">
-    {{--문항 박스 : div--}}
+
+    <div id="quizTitle" style="margin-left: 5%" class="form-inline">
+        <button type="button" class="btn btn-primary btn-lg outline">퀴즈명 : {{$response['listName']}}</button>
+    </div>
+
+    <!--문제 박스 : div-->
     <div class="quizBox">
 
     </div>
 
     <div class="two_button">
+        <button type="button" class="btn btn-primary" onclick="backToList()">목록으로</button>
         <button type="button" class="btn btn-primary" id="save">저장</button>
         <button type="button" class="btn btn-primary" id="add">추가</button>
     </div>
