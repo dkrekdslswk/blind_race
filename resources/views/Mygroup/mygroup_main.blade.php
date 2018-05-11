a83a5a799527f565828fc617e1685a7bc965ae5a
LilMinsu <smilemsu@naver.com>
2018-05-12 01:10:03 +0900

    추가 수정중

--- resources/views/Mygroup/mygroup_main.blade.php
+++ resources/views/Mygroup/mygroup_main.blade.php
@@ -13,36 +13,7 @@
     <link
             rel="stylesheet"
             href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script>
-        //그룹ID
 
-        function add_student(st_made_number){
-            var student_number = $("#st"+st_made_number).text();
-
-            var student_number_zip = new Array();
-
-            //배열을 push 할 경우는 [["13","14","15"],"19","18"] 이런식으로 2차원으로 들어가 처리가 더필요함
-            student_number_zip.push(student_number);
-
-
-            student_number_zip = JSON.stringify(student_number_zip);
-
-            $.ajax({
-                type: 'POST',
-                url: "{{url('/groupController/pushInvitation')}}",
-                dataType: 'json',
-                headers: { 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content') },
-                data: "groupId="+pub_groupId+"&students="+student_number_zip,
-                success: function (data) {
-                    alert("성공");
-                },
-                error: function (data) {
-                    alert("에러");
-                }
-            });
-        }
-
-    </script>
     <style>
         body {
             font-family: arial, sans-serif;
@@ -147,10 +118,13 @@
 
     }
 
+    //그룹ID
 
-
-
-    function add_student(st_made_number){
+    function add_student(){
+        alert('시발');
+        var userId = document.getElementById("GroupData['users']").value;
+        var userName = document.getElementById("studentnames").value;
+        alert('시발');
         var student_number = $("#st"+st_made_number).text();
 
         var student_number_zip = new Array();
@@ -175,6 +149,9 @@
             }
         });
     }
+
+
+
 
     $(document).ready(function () {
 
