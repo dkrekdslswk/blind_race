05d8246ff5134018fe2799505bb4508c0c82ecaf
dkrekdslswk <32236174+dkrekdslswk@users.noreply.github.com>
2018-05-14 20:43:21 +0900

    RecordBoxController
    구문 수정

--- app/Http/Controllers/RecordBoxController.php
+++ app/Http/Controllers/RecordBoxController.php
@@ -567,8 +567,8 @@ class RecordBoxController extends Controller{
                 're.retest' => 0,
                 'r.groupNumber' => $groupId
             ])
-            ->where(DB::raw('date(r.created_at)'), '>=', DB::raw('date('.$startDate.')'))
-            ->where(DB::raw('date(r.created_at)'), '<=', DB::raw('date('.$endDate.')'))
+            ->where('date(r.created_at)', '>=', $startDate)
+            ->where('date(r.created_at)', '<=', $endDate)
             ->join('lists as l', 'l.number', '=', 'r.listNumber')
             ->join('raceUsers as ru', 'ru.raceNumber', '=', 'r.number')
             ->join('records as re', function ($join){
