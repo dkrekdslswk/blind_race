b6ceab66df19897fd0b310a85e632ed0d8d9d805
PheonixAhn <ajk78978@naver.com>
2018-08-06 19:44:58 +0900

    레이스 답 색상변경

--- resources/views/Race/race_student_content.blade.php
+++ resources/views/Race/race_student_content.blade.php
@@ -156,23 +156,23 @@
             <div class="obj" style="display:none;">
 
                 <!-- 1번 -->
-                <button class="column btn-success" onclick="web_answer(1);" style="background-image:url('/img/race_play/answer_a.png');">
+                <button class="column btn-success" onclick="web_answer(1);" style="background-color:#8461bd;">
                     <!-- style="background-color:#1bbc9b;" -->
                     <p class="answer_font" id="answer1">A</p>
                 </button>
 
                 <!-- 2번 -->
-                <button class="column btn-primary" onclick="web_answer(2);" style="background-image:url('/img/race_play/answer_b.png');">
+                <button class="column btn-primary" onclick="web_answer(2);" style="background-color:#e74d66;">
                     <p class="answer_font" id="answer2">B</p>
                 </button>
 
                 <!-- 3번 -->
-                <button class="column btn-warning" onclick="web_answer(3);" style="background-image:url('/img/race_play/answer_c.png');">
+                <button class="column btn-warning" onclick="web_answer(3);" style="background-color:#44d193;">
                     <p class="answer_font" id="answer3">C</p>
                 </button>
 
                 <!-- 4번 -->
-                <button class="column btn-danger" onclick="web_answer(4);" style="background-image:url('/img/race_play/answer_d.png');">
+                <button class="column btn-danger" onclick="web_answer(4);" style="background-color:#ffb22c;" >
                     <p class="answer_font" id="answer4">D</p>
                 </button>
             </div>
